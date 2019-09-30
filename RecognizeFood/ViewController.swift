b64c134c1bb6a951b419761f2eef4f31d930ecaf
Lena Sabadina <elena.sabadina@icloud.com>
2019-09-30 11:11:45 +0200

    Initial Commit

---
+++ RecognizeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecognizeFood
+//
+//  Created by Lena Sabadina on 2019-09-30.
+//  Copyright © 2019 Whiskerz AB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
